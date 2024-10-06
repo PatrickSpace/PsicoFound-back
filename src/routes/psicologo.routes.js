e426443d45125d7eaa5dacdcec9cbdee19526b9c
PatrickSpace <jplosta01@gmail.com>
2024-10-05 21:35:41 -0500

    first route

---
+++ src/routes/psicologo.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const psicologoController = require("../controllers/psicologo.controller");
+
+router.get("/", psicologoController.findall);
+router.get("/:id", psicologoController.findById);
+router.post("/", psicologoController.create);
+router.put("/", psicologoController.updated);
+router.delete("/", psicologoController.delete);
